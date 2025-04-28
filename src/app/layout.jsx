f45e38cd4997ca95cb5c2146b0bf067a24730194
shifat-arman <shifatshiam@gmail.com>
2025-04-28 18:15:16 +0600

    deleted zone identifiers

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,16 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IEEEAIUBSB',
-  description: 'Created by IEEE AIUB Student Branch Web Development Team.',
-}
+  title: "IEEEAIUBSB",
+  description: "Created by IEEE AIUB Student Branch Web Development Team.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
-  )
+  );
 }
